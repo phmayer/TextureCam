f228efd54a7638ed347abc6a04ed51c95cec82d8
phmayer <phmayer@stud.hs-heilbronn.de>
2023-12-17 03:28:10 +0900

    added a wayyyyy better keybaord .....but code is still very wip and kinda is a hot mess and needs cleanup etc...

--- python_code/src/gui/wifidialog.py
+++ python_code/src/gui/wifidialog.py
@@ -1,7 +1,7 @@
 import pygame
-import vkeyboard
-import keyboard
+
 import pygame_textinput
+from pygame_vkeyboard import *
 
 from app_buttons import Button
 
@@ -9,28 +9,56 @@ from app_buttons import Button
 #constants:
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 480
 
+#global variables:
+textinput1 = ''
+
 
 #functions:
+
+def consumer(text):
+    textinput1 = '%s' % text
+    print('Current text : %s' % text)
+
 #button:
 def button_action():
     print("Button released - Function executed!")
-    overlay_screen.fill("green")
+    overlay_screen.fill("white")
     pygame.display.flip()
-    kb.start()
+    #kb.start() #TODO probably get rid of that on screen bs keybaord shit
+
+    # Initializes and activates vkeyboard
+    layout = VKeyboardLayout(VKeyboardLayout.AZERTY)
+    keyboard = VKeyboard(overlay_screen, consumer, layout)
+
+    #create some text field bullshit:
+    font = pygame.font.Font('freesansbold.ttf', 32)
+    
 
     while running:
         events = pygame.event.get()
 
+        keyboard.update(events)
+        keyboard.draw(overlay_screen)
         # Feed it with events every frame
-        textinput_gdrive.update(events)
+        #textinput_gdrive.update(events)
         # Blit its surface onto the screen
-        overlay_screen.blit(textinput_gdrive.surface, (400, 200))
-        pygame.display.update()
+        #overlay_screen.blit(textinput_gdrive.surface, (400, 200))
+
+
+        text = font.render(textinput1, True, "black", "pink")
+        textfield = text.get_rect() #TODO HOW THE FUCK DO I MAKE THIS UPDATE ITSELF?????
+        textfield.center = (400, 100)
+        overlay_screen.blit(text,textfield)
+
+
+        pygame.display.update() #WHAT TGE FUKC which one do i need?
+        pygame.display.flip()
 
         for event in events:
             if event.type == pygame.QUIT:
                 exit()
-        clock.tick(30)
+        #clock.tick(30) #TODO what the hell am i supposed to put here do it need this?
+        # i hate pygame
 
     
 
@@ -50,10 +78,6 @@ camera_button  = Button(pygame_screen,200,200,cameraButton_up,cameraButton_down,
 # Create TextInput-object
 textinput_gdrive = pygame_textinput.TextInputVisualizer()
 
-
-#keyboard setup
-kb = vkeyboard.VirtualKeyboard()
-kb.engine()
 
 
 
